c8eb60756c4de6827c40562733fe225f7d4579fc
hss <hong837198@gmail.com>
2024-12-14 22:10:44 +0900

    git modify

--- src/main/java/com/example/gitstudy/GitStudyApplication.java
+++ src/main/java/com/example/gitstudy/GitStudyApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitStudyApplication {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println();
+        System.out.println("a");
+        System.out.println("a");
         SpringApplication.run(GitStudyApplication.class, args);
     }
 

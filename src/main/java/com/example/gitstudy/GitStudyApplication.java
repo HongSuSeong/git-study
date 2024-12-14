9329f648cf806c5b61ef3d5dc904b9ce35de232b
hss <hong837198@gmail.com>
2024-12-14 21:38:12 +0900

    test

--- src/main/java/com/example/gitstudy/GitStudyApplication.java
+++ src/main/java/com/example/gitstudy/GitStudyApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitStudyApplication {
-
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(GitStudyApplication.class, args);
     }
 
